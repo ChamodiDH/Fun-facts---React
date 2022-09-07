bc211c5ac5463f17e5dab2edb6633d4709370025
ChamodiDH <chamodi3797@gmail.com>
2022-09-07 21:49:50 +0530

    Implementation

---
+++ Project1/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+export default function Navbar() {
+    return( 
+
+    <div className="container1">
+    <div className="subc1">
+     <img id="logo" src="/vite.svg"/>
+    <h2>React facts</h2>
+    </div>
+    <h3>React Course - Project 1</h3>
+    </div>
+    
+    )
+}
